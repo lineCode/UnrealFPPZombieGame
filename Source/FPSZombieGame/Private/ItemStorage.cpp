ecfbc245b59afd7d6b17e2ef39e7222b914f5540
Patryk Basa <patryk9595@gmail.com>
2022-11-17 21:10:56 +0100

    Interface abstraction for items pickup

---
+++ Source/FPSZombieGame/Private/ItemStorage.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemStorage.h"
+
+// Add default functionality here for any IItemStorage functions that are not pure virtual.
