a77a1d455afc1dd0964eb73ce85938919c5eecc2
Patryk Basa <patryk9595@gmail.com>
2022-11-22 14:41:12 +0100

    removed old implementation

---
+++ Source/FPSZombieGame/Private/PickupItemSystem/Usabilities/ShootProjectile.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickupItemSystem/Usabilities/ShootProjectile.h"
+
+#include "FPSZombieGameCharacter.h"
+#include "FPSZombieGameProjectile.h"
+#include "Kismet\GameplayStatics.h"
+
+void UShootProjectile::Use(AActor* User)
+{
+	AFPSZombieGameCharacter* Character = Cast<AFPSZombieGameCharacter>(User);
+
+	if (Character == nullptr || Character->GetController() == nullptr)
+	{
+		return;
+	}
+
+	// Try and fire a projectile
+	if (ProjectileClass != nullptr)
+	{
+		UWorld* const World = GetWorld();
+		if (World != nullptr)
+		{
+			APlayerController* PlayerController = Cast<APlayerController>(Character->GetController());
+			const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
+			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+			const FVector SpawnLocation = Character->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset);
+
+			//Set Spawn Collision Handling Override
+			FActorSpawnParameters ActorSpawnParams;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+			// Spawn the projectile at the muzzle
+			World->SpawnActor<AFPSZombieGameProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+		}
+	}
+
+	// Try and play the sound if specified
+	if (FireSound != nullptr)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
+	}
+
+	// Try and play a firing animation if specified
+	if (FireAnimation != nullptr)
+	{
+		// Get the animation object for the arms mesh
+		UAnimInstance* AnimInstance = Character->GetMeshWithSockets()->GetAnimInstance();
+		if (AnimInstance != nullptr)
+		{
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
+}
---
+++ Source/FPSZombieGame/Public/PickupItemSystem/Usabilities/ShootProjectile.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupItemSystem\Usable.h"
+#include "UObject/NoExportTypes.h"
+#include "ShootProjectile.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract,Blueprintable)
+class FPSZOMBIEGAME_API UShootProjectile : public UObject, public IUsable
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, Category=Projectile)
+	TSubclassOf<class AFPSZombieGameProjectile> ProjectileClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay, meta=(AllowPrivateAccess = "true"))
+	FVector MuzzleOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay, meta=(AllowPrivateAccess = "true"))
+	USoundBase* FireSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay, meta=(AllowPrivateAccess = "true"))
+	UAnimMontage* FireAnimation;
+
+public:
+
+	virtual void Use(AActor* User) override;
+};
